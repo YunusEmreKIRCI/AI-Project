d7dccb3fa6227a333bae5fca67fa37b6d1f79bba
YunusEmreKIRCI <81527241+YunusEmreKIRCI@users.noreply.github.com>
2021-05-15 23:06:11 +0300

    Update AIödev 15 mayıs .py

--- AIödev 15 mayıs .py
+++ AIödev 15 mayıs .py
@@ -1,10 +1,12 @@
 metin = open('odev.txt')
 yeni_metin = ""
 for x in metin:
+    satir=""
     y = list(x)
     for a in y:
         if a.isalpha():
+            satir += a
             yeni_metin += a
-
+    print(satir)
 print(yeni_metin)
 
